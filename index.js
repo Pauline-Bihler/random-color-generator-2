eafbd39f89a1a2948b009d0a478177ff3ebdacc7
Pauline Bihler <pauline.erika5@gmail.com>
2023-09-07 16:44:22 +0200

    Fix index.js code

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ import chalk from 'chalk';
 
 const color = randomColor({ hue: argv[2], luminosity: argv[3] });
 
-// code for the first 3 rows
+// Code for the first 3 rows
 const hashToRepeat = '###############################';
 for (let i = 0; i < 3; i++) {
   console.log(chalk.hex(color).bold(hashToRepeat));
 }
 
-// code for the next 3 rows but with conditions
+// Code for the next 3 rows with conditions
 const hashToRepeatWithSpaces = `#####                     #####`;
+
 const hashMiddle = `#####       ${color}       #####`;
 
-for (let row = 0; row < 4; row++) {
+for (let row = 1; row <= 3; row++) {
   if (row === 1 || row === 3) {
     console.log(chalk.hex(color).bold(hashToRepeatWithSpaces));
   } else if (row === 2) {
@@ -22,8 +23,8 @@ for (let row = 0; row < 4; row++) {
   }
 }
 
-// code for the last 3 rows ~ identical to the code of the first 3 rows.
+// Code for the last 3 rows, identical to the code for the first 3 rows
 const hashToRepeatBottom = '###############################';
 for (let i = 0; i < 3; i++) {
-  console.log(chalk.hex(color).bold(hashToRepeat));
+  console.log(chalk.hex(color).bold(hashToRepeatBottom));
 }
